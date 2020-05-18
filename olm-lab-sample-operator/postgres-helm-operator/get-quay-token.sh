353e8e4b7cabcc4115881a5a5f3311cf9a161c61
Deeksha Hegde <deeksha.h@gmail.com>
2020-05-18 08:22:49 -0500

    Added script to fetch quay token

---
+++ olm-lab-sample-operator/postgres-helm-operator/get-quay-token.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+
+set -e
+
+
+
+echo -n "Username: "
+
+read USERNAME
+
+echo -n "Password: "
+
+read -s PASSWORD
+
+echo
+
+
+
+TOKEN_JSON=$(curl -s -H "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '
+
+{
+
+    "user": {
+
+        "username": "'"${USERNAME}"'",
+
+        "password": "'"${PASSWORD}"'"
+
+    }
+
+}')
+
+
+
+TOKEN=`echo $TOKEN_JSON | awk '{split($0,a,"\""); print a[4]}'`
+
+
+
+echo ""
+
+echo "========================================"
+
+echo "Auth Token is: ${TOKEN}"
+
+echo "========================================"
